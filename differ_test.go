213f86f147446f579c97b149a24b860b0670c8b1
SatoShun <shun.sato1@gmail.com>
2017-01-02 14:05:43 +0000

    diff test and codecov

--- differ_test.go
+++ differ_test.go
@@ -18,3 +18,21 @@ func TestStoreInCache(t *testing.T) {
 		t.Error("not throw error when file not exists")
 	}
 }
+
+func TestDiff(t *testing.T) {
+	cache = make(map[string]string)
+	cache["./differ_test.go"] = "hoge"
+
+	item := diff("./differ_test.go")
+	if item.Diff == "" {
+		t.Error("no diff")
+	}
+	if item.Diff[0:5] != "--- /" {
+		t.Error("wrong prefix")
+	}
+
+	item = diff("./differ_test.go")
+	if item.Diff != "" {
+		t.Error("diff")
+	}
+}
